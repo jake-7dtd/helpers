d95b55267d6b63d0e631b066f5a7935eedb7e872
jake-7dtd <111293883+jake-7dtd@users.noreply.github.com>
2022-09-05 14:48:59 +0200

    code upgrade

--- generate-prefab-info.sh
+++ generate-prefab-info.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #Copy the file to a directory containing a generated world and launch. Linux or Cygwin only, this does not run under Windows!
-awk '{ print $4" "$6 }' FS=\" prefabs.xml > prefabLoc.lst
-awk '{ print $4 }' FS=\" prefabs.xml > prefab.lst
-sort prefab.lst | uniq -c > prefab.num
-sort --reverse prefab.num > prefab.sorted
-grep part_ prefab.sorted > prefab.parts-only.sorted
-grep -v part_ prefab.sorted > prefab.no-parts.sorted
-grep -v rwg_tile prefab.no-parts.sorted > prefab.no-parts.no-tiles.sorted
-grep rwg_tile prefab.no-parts.sorted > prefab.tiles-only.sorted
+awk '{print $4" "$6}' FS=\" prefabs.xml | grep -v "^UTF-8 $" | awk 'NF' | awk '$1=$1' > prefabs.loc.lst
+awk '{print $4}' FS=\" prefabs.xml | grep -v "^UTF-8$" | awk 'NF' | awk '$1=$1' > prefabs.lst
+sort prefabs.lst | uniq -c > prefabs.num
+sort --reverse prefabs.num > prefabs.sorted
+rm prefabs.num prefabs.lst
+grep part_ prefabs.sorted > prefabs.parts-only.sorted
+grep -v part_ prefabs.sorted > prefabs.no-parts.sorted
+grep rwg_tile prefabs.no-parts.sorted > prefabs.tiles-only.sorted
+grep -v rwg_tile prefabs.no-parts.sorted > prefabs.no-parts.no-tiles.sorted
