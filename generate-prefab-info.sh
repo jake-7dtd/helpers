b2fc6089319cf8b7bd4ab72fe944d85b8b24acdb
jake-7dtd <111293883+jake-7dtd@users.noreply.github.com>
2022-09-05 14:01:19 +0200

    field separation with spaces

--- generate-prefab-info.sh
+++ generate-prefab-info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Copy the file to a directory containing a generated world and launch. Linux or Cygwin only, this does not run under Windows!
-awk '{ print $4 $6 }' FS=\" prefabs.xml > prefabLoc.lst
+awk '{ print $4" "$6 }' FS=\" prefabs.xml > prefabLoc.lst
 awk '{ print $4 }' FS=\" prefabs.xml > prefab.lst
 sort prefab.lst | uniq -c > prefab.num
 sort --reverse prefab.num > prefab.sorted
