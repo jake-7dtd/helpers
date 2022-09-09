77a6e33c68b780eb43ed5de65cfa29a0dbd76c48
jake-7dtd <111293883+jake-7dtd@users.noreply.github.com>
2022-09-09 22:35:42 +0200

    code update

--- generate-prefab-info.sh
+++ generate-prefab-info.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Copy the file to a directory containing a generated world and launch. Linux or Cygwin only, this does not run under Windows!
-awk '{print $4" "$6}' FS=\" prefabs.xml | grep -v "^UTF-8 $" | awk 'NF' | awk '$1=$1' > prefabs.loc.lst
 awk '{print $4}' FS=\" prefabs.xml | grep -v "^UTF-8$" | awk 'NF' | awk '$1=$1' > prefabs.lst
 sort prefabs.lst | uniq -c > prefabs.num
 sort --reverse prefabs.num > prefabs.sorted
@@ -9,3 +8,6 @@ grep part_ prefabs.sorted > prefabs.parts-only.sorted
 grep -v part_ prefabs.sorted > prefabs.no-parts.sorted
 grep rwg_tile prefabs.no-parts.sorted > prefabs.tiles-only.sorted
 grep -v rwg_tile prefabs.no-parts.sorted > prefabs.no-parts.no-tiles.sorted
+
+awk '{print $4" "$6}' FS=\" prefabs.xml | grep -v "^UTF-8 $" | awk 'NF' | awk '$1=$1' > prefabs.loc.lst
+grep -v part_ prefabs.loc.lst | grep -v rwg_tile > prefabs.loc.no-parts.no-tiles.lst
